70655c7ba50c96d152528638ec33ac4e27c83652
lexblol <103933352+lexblol@users.noreply.github.com>
2024-10-12 15:01:06 +0000

    Add files via upload

---
+++ Loteria.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Random;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		System.out.printf("Bienvenido a la Lotería!%n");
+		
+		Scanner scan = new Scanner(System.in);
+		Random rand = new Random();
+
+		//Apuesta del usuario:
+
+		Set<Integer> apuesta = new HashSet<Integer>();
+		
+		for (int i = 0; i < 6; i++) {
+			String[] orden = {"Primer", "Segundo", "Tercer", "Cuarto", "Quinto", "Sexto"};
+			System.out.printf("%nIntroduzca %s número de su apuesta (1 a 49): ", orden[i]);
+			int num = scan.nextInt();
+
+			if (num > 49 || num < 1) {
+				System.out.printf("%nNúmero incorrecto; solamente del 1 al 49. ");
+				i = i - 1;
+				continue;
+			} else if (apuesta.contains(num)) {
+				System.out.printf("%nNúmero ya introducido. ");
+				i = i - 1;
+				continue;
+			} else {
+				apuesta.add(num);
+			}
+		}
+			
+		System.out.printf("%nSu apuesta: " + apuesta);
+
+		//Números ganadores:
+		
+		Set<Integer> ganadores = new HashSet<Integer>();
+
+		for (int i = 1; i < 7; i++) {
+			int num = rand.nextInt(49) + 1;
+
+			if (ganadores.contains(num)) {
+				i = i - 1;
+				continue;
+			} else {
+				ganadores.add(num);
+			}
+		}
+		
+		System.out.printf("%nLos números ganadores: " + ganadores);
+
+		//Aciertos:
+		
+		apuesta.retainAll(ganadores);
+
+		System.out.printf("%nAciertos: " + apuesta.size() + ": " + apuesta + "%n");
+	}
+}
